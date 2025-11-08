cdc7f7c9aed8b4ea56c8e6c312cb44929df9daa4
Glacequb <david.aeschlimann@gmail.com>
2025-11-08 09:57:01 +0100

    generating the playstationscrape result into a json

--- Deel1-Puppeteer/index.js
+++ Deel1-Puppeteer/index.js
@@ -1,3 +1,6 @@
 import { scrapePlaystations } from "./playstation.js";
+import fs from "fs";
 
-scrapePlaystations();
+// create JSON with filtered playstation products over 600 euro
+const filteredProducts = await scrapePlaystations();
+fs.writeFileSync("playstations.json", JSON.stringify(filteredProducts, null, 2), "utf-8");
--- Deel1-Puppeteer/playstation.js
+++ Deel1-Puppeteer/playstation.js
@@ -1,33 +1,34 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer"
 
 // Scrape Playstations
 async function scrapePlaystations() {
-  const browser = await puppeteer.launch({ headless: 'new' });
-  const page = await browser.newPage();
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36');
-  await page.goto('https://www.coolblue.be/nl/consoles/playstation5', { waitUntil: 'networkidle0' });
+	const browser = await puppeteer.launch({ headless: "new" })
+	const page = await browser.newPage()
+	await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+	await page.goto("https://www.coolblue.be/nl/consoles/playstation5", { waitUntil: "networkidle0" })
 
-  const pageTitle = await page.$eval('.filtered-search__header h1', (element) => element.textContent.trim());
+	const pageTitle = await page.$eval(".filtered-search__header h1", (element) => element.textContent.trim())
 
-  const products = await page.$$eval('.product-card', (rows) => {
-    return rows.map((row) => ({
-      productTitle: row.querySelector('.product-card__title').textContent.trim(),
-      price: row.querySelector('.sales-price__current').textContent.trim(),
-       beschikbaarheid: (
-         row.querySelector('.color--available, .color--unavailable')?.textContent.trim() || ''
-       ),
-    }));
-  });
+	const products = await page.$$eval(".product-card", (rows) => {
+		return rows.map((row) => ({
+			productTitle: row.querySelector(".product-card__title").textContent.trim(),
+			price: row.querySelector(".sales-price__current").textContent.trim(),
+			beschikbaarheid: row.querySelector(".color--available, .color--unavailable")?.textContent.trim(),
+		}))
+	})
 
-  const filteredProducts = products.filter((product) => {
-    product.price > 600;
-  });
+	const filteredProducts = products.filter((product) => {
+		const priceFloat = parseFloat(product.price.replace(/[^\d,]/g, "").replace(",", "."))
+		return priceFloat > 600
+	})
 
-  console.log(pageTitle);
-  console.log(products);
-  console.log(filteredProducts);
+	console.log(pageTitle)
+	console.log(products)
+	console.log(filteredProducts)
 
-  await browser.close();
+	await browser.close()
+
+	return filteredProducts
 }
 
-export { scrapePlaystations };
+export { scrapePlaystations }
